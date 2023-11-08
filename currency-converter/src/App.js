5e944cf9c5ad59fd491456a3471284d3f1270bdd
tarikkurtanovic <tarik2371@gmail.com>
2023-11-08 12:31:31 +0100

    Currency converter task done

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -3,42 +3,77 @@
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [input, setInput] = useState(200);
-  // const [currency, setCurrency] = useState(null);
-  // const [currency2, setCurrency2] = useState(null);
+  const [amount, setAmount] = useState(0);
+  const [currency, setCurrency] = useState("EUR");
+  const [currency2, setCurrency2] = useState("USD");
+  const [outcome, setOutcome] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(function () {
-    async function fetchingCurrency() {
-      const response = await fetch(
-        `https://api.frankfurter.app/latest?amount=${input}&from=EUR&to=USD`
-      );
-      const data = await response.json();
-      console.log(data);
-    }
-    fetchingCurrency();
-  }, []);
+  // function handleResetInput() {
+  //   setAmount(0);
+  // }
+
+  function handleReset() {
+    setAmount(0);
+    setCurrency("EUR");
+    setCurrency2("USD");
+    setOutcome("");
+  }
+
+  useEffect(
+    function () {
+      async function fetchingCurrency() {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=${currency2}`
+        );
+        const data = await response.json();
+        setOutcome(data.rates[currency2]);
+        setIsLoading(false);
+      }
+      if (currency === currency2) return setOutcome(amount);
+      fetchingCurrency();
+      // handleResetInput();
+    },
+
+    [amount, currency, currency2]
+  );
 
   return (
     <div>
       <input
         type="text"
-        onChange={(e) => setInput(+e.target.value)}
-        value={input}
+        onChange={(e) => setAmount(+e.target.value)}
+        value={amount}
+        disabled={isLoading}
       />
+      <select
+        value={currency}
+        onChange={(e) => setCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
 
-      <select>
+      <select
+        value={currency2}
+        onChange={(e) => setCurrency2(e.target.value)}
+        disabled={isLoading}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p>
+
+      <p>
+        {outcome} {currency2}
+      </p>
+
+      <button onClick={handleReset}>RESET</button>
     </div>
   );
 }
