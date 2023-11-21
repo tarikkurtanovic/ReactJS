8ff1b99a6a0b5be9804ef7f83adecfb203675b94
tarikkurtanovic <tarik2371@gmail.com>
2023-11-21 12:37:08 +0100

    Using CSS Modules

--- 11. WorldWise project - Vite/src/pages/Homepage.jsx
+++ 11. WorldWise project - Vite/src/pages/Homepage.jsx
@@ -1,13 +1,16 @@
 import { NavLink } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   <PageNav />;
 
   return (
     <div>
+      <PageNav />
+      <AppNav />
       <h1>Worldwise</h1>
-      <NavLink to="/pricing">Pricing </NavLink>
+      <NavLink to="/app">Go to the app</NavLink>
     </div>
   );
 }
